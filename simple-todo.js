22347e1174da4357599c2921671fc1a08549f206
Fernando L. Canizo <fernandocanizo@gmail.com>
2016-03-22 07:36:05 -0300

    Add insert new tasks capability

--- simple-todo.js
+++ simple-todo.js
@@ -9,7 +9,19 @@ var Tasks = new Mongo.Collection('tasks');
 if (Meteor.isClient) {
 	Template.body.helpers({
 		tasks: function () {
-			return Tasks.find({});
+			return Tasks.find({}, {sort: {createdAt: -1}});
+		}
+	});
+
+	Template.body.events({
+		'submit .new-task': function (event) {
+			event.preventDefault();
+			var text = event.target.text.value;
+			Tasks.insert({
+				text: text,
+				createdAt: new Date()
+			});
+			event.target.text.value = "";
 		}
 	});
 }
