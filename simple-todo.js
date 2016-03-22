be072592ff25d4d480d7d913f1c0a3c20264427f
Fernando L. Canizo <fernandocanizo@gmail.com>
2016-03-22 07:15:44 -0300

    Change basic starting app into a todo-app

--- simple-todo.js
+++ simple-todo.js
@@ -1,23 +1,20 @@
+"use strict";
+
+/* global Meteor */
+/* global Template */
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+	Template.body.helpers({
+		tasks: [
+			{text: 'task 1'},
+			{text: 'task 2'},
+			{text: 'task 3'}
+		]
+	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function () {
+	// code to run on server at startup
+	});
 }
